ac91c7497099cf1eb1ac22a0aa293a9e8f9f595b
Alexey Kumin <a.kumin@mail.com>
2017-11-23 11:52:04 +0300

    stack

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,112 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+//#ifndef ___STACK_H___
+//#define ___STACK_H___
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX_STACK_SIZE = 1000;
+
+//Шаблон стека
+template <class ValType>
+class Stack
+{
+protected:
+	ValType *pStack;  // указатель на стек
+	int Size;  // максимальное количество элементов в стеке
+	int top;  // номер текущего элемента стека
+public:
+	Stack(int s = 10);  //коструктор, по умолчанию размер стека 10
+	Stack(const Stack &st); // конструктор копирования
+	~Stack();  // деструктор
+
+	void push(const ValType &a);  //  вставка элемента
+	ValType pop();  // извлечение элемента
+	ValType view() { return pStack[top - 1]; };  // просмотр верхнего элемента
+	bool isempty() { return top == 0; };  // проверка на пустоту
+	int getTop() { return top; }  // получение кол-ва элементов
+	int getSize() { return Size; } //доступ к размеру
+	void clear() { top = 0; } // очистка стека
+};/*-------------------------------------------------------------------------*/
+
+// конструктор
+template <class ValType>
+Stack<ValType>::Stack(int s)
+{
+	if ((s >= 0) && (s <= MAX_STACK_SIZE))
+	{
+		Size = s;
+		top = 0;
+		pStack = new ValType[Size];
+	}
+	else
+		throw "SizeError";
+}/*-------------------------------------------------------------------------*/
+
+// конструктор копирования
+template <class ValType>
+Stack<ValType>::Stack(const Stack &st)
+{
+	Size = st.Size;
+	top = st.top;
+	pStack = new ValType[Size];
+	for (int i; i < top; i++)
+		pStack[i] = st.pStack[i];
+}/*-------------------------------------------------------------------------*/
+
+// деструктор
+template <class ValType>
+Stack<ValType>::~Stack()
+{
+	delete[] pStack;
+}/*-------------------------------------------------------------------------*/
+
+// вставка элемента(с перевыделением памяти)
+template <class ValType>
+void Stack<ValType>::push(const ValType &a)
+{
+	if (top < Size)
+	{
+		top++;
+		pStack[top - 1] = a;
+	}
+	else
+	{
+		if ((Size + (Size + 2) / 2) <= MAX_STACK_SIZE)
+		{
+			Size = Size + (Size + 2) / 2;
+			ValType* Temp = new ValType[Size];
+			for (int i = 0; i < top; i++)
+				Temp[i] = pStack[i];
+			delete[] pStack;
+			pStack = Temp;
+			top++;
+			pStack[top - 1] = a;
+		}
+		else
+			throw "SizeError";
+	}
+}/*-------------------------------------------------------------------------*/
+
+ // извлечение элемента
+template <class ValType>
+ValType Stack<ValType>::pop()
+{
+	if (top != 0)
+	{
+		top--;
+		return pStack[top];
+	}
+	else
+		throw "SizeError";
+}/*-------------------------------------------------------------------------*/
+ 
+
+ 
+
+
+
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,72 @@
 
 #include "stack.h"
 #include <gtest.h>
+
+class TestStacks : public ::testing::Test {
+protected:
+	Stack<int> st1;
+public:
+	TestStacks() : st1(10) {
+		for (int i = 0; i < st1.getSize(); i++)
+			st1.push(i);	
+	}
+	~TestStacks() {}
+};
+
+
+TEST(Stack, can_create_stack_with_positive_lenght)
+{
+	ASSERT_NO_THROW(Stack<int> st(5));
+}
+
+TEST(Stack, can_create_stack_with_lenght_more_max)
+{
+	ASSERT_ANY_THROW(Stack<int> st(6000));
+}
+
+TEST(Stack, can_create_stack_with_negative_lenght)
+{
+	ASSERT_ANY_THROW(Stack<int> st(-5));
+}
+
+TEST(Stack, can_not_delete_if_stack_is_empty)
+{
+	Stack<int> st;
+	ASSERT_ANY_THROW(st.pop());
+}
+
+//TEST_F(TestStacks, copied_stack_is_equal_to_source_one)
+//{
+//	Stack<int> st(st1);
+//	EXPECT_EQ(st1, st);
+//}
+
+TEST_F(TestStacks, can_get_quantity)
+{
+	EXPECT_EQ(10, st1.getSize());
+}
+
+TEST_F(TestStacks, can_push_element_and_view_top)
+{
+	st1.push(0);
+	//EXPECT_EQ(11, st1.getTop());
+	EXPECT_EQ(0, st1.view());
+}
+
+TEST_F(TestStacks, can_pop_element)
+{
+	int s = st1.pop();
+	EXPECT_EQ(9, s);
+	EXPECT_EQ(9, st1.getTop());
+}
+
+TEST_F(TestStacks, can_check_emptiness)
+{
+	EXPECT_EQ(false, st1.isempty());
+}
+
+TEST_F(TestStacks, can_clean_steck)
+{
+	st1.clear();
+	EXPECT_EQ(true, st1.isempty());
+}
